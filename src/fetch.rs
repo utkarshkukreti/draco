2dfd569c3b7d49e359fe38a5558d99e6047dd6f2
qthree <qthree3@gmail.com>
2018-11-20 16:53:29 +0700

    Add ability to POST with body and Content-Type

--- src/fetch.rs
+++ src/fetch.rs
@@ -7,6 +7,8 @@ use web_sys as web;
 pub struct Request {
     method: String,
     url: String,
+    body: Option<JsValue>,
+    content_type: Option<&'static str>
 }
 
 impl Request {
@@ -14,13 +16,30 @@ impl Request {
         Request {
             method: method.into(),
             url: url.into(),
+            body: None,
+            content_type: None
+        }
+    }
+    pub fn with_body(method: &str, url: &str, body: JsValue, content_type: &'static str) -> Request {
+        Request {
+            method: method.into(),
+            url: url.into(),
+            body: Some(body),
+            content_type: Some(content_type),
         }
     }
 
     pub fn send<R: Response>(self) -> impl Future<Item = R::Item, Error = Error> {
         let mut init = web::RequestInit::new();
         init.method(&self.method);
+        if let Some(body) = self.body.as_ref() {
+            init.body(Some(body));
+        }
+
         let request = web::Request::new_with_str_and_init(&self.url, &init).unwrap();
+        if let Some(content_type) = self.content_type {
+            request.headers().set("Content-Type", content_type).unwrap();
+        }
         let promise = web::window().unwrap().fetch_with_request(&request);
         R::send(JsFuture::from(promise).map(|response| {
             assert!(response.is_instance_of::<web::Response>());
@@ -61,4 +80,13 @@ impl Response for Text {
 
 pub fn get(url: &str) -> Request {
     Request::new("GET", url)
+}
+
+pub fn post(url: &str) -> Request {
+    Request::new("POST", url)
+}
+
+pub fn post_json(url: &str, json: &str) -> Request {
+    let body = JsValue::from_str(json);
+    Request::with_body("POST", url, body, "application/json")
 }
