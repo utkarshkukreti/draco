3f75995f0af3d16c4732704a5842478d7b9aa450
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-10-14 10:16:15 +0530

    feat(draco::dbg!): copied from std and modified to log to console

--- src/console.rs
+++ src/console.rs
@@ -28,3 +28,27 @@ macro_rules! time {
         value
     }};
 }
+
+// From https://github.com/rust-lang/rust/blob/36d4506cc64337bf7dfc1e3120156922e97659c7/src/libstd/macros.rs#L336-L358
+#[macro_export]
+macro_rules! dbg {
+    () => {
+        $crate::log!("[{}:{}]", file!(), line!());
+    };
+    ($val:expr) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::log!("[{}:{}] {} = {:#?}",
+                    file!(), line!(), stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    // Trailing comma with single argument is ignored
+    ($val:expr,) => { $crate::dbg!($val) };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
