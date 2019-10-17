218c861e6984d0aa8637b6be65062ec5d9b188ea
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-10-17 13:28:54 +0530

    docs(examples/hello_world): improve the documentation

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
-// A Draco application requires a struct to store its state.
+// A Draco application is a struct (or enum) which stores its state.
 // We declare a unit struct here because we don't have any state to store.
 struct HelloWorld;
 
@@ -8,22 +8,24 @@ struct HelloWorld;
 impl draco::App for HelloWorld {
     // `Message` is the type of value our HTML will emit.
     // Here we aren't emitting anything so we use the unit type.
-    // You can put any type here and this will still compile.
+    // You can put any type here and this example will still compile.
     type Message = ();
 
     // The `view` function returns what we want to display on the page.
     fn view(&self) -> draco::Node<Self::Message> {
-        // `draco::html` contains functions to create HTML elements.
         // `draco::html::h1()` creates an `<h1>` element.
-        // `.push()` adds a child to the element. Here we add a Text child by pushing a string.
-        // We use `.into()` at the end to convert an `Element` struct to a `Node` struct which
-        // this function must return.
-        draco::html::h1().push("Hello, world!").into()
+        draco::html::h1()
+            // `.push()` adds a child to the element. Here we add a Text Node by pushing a string.
+            .push("Hello, world!")
+            // We use `.into()` to convert an `Element` struct to a `Node` struct which this
+            // function must return.
+            .into()
     }
 }
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    // We select the first `<main>` in the page and start running the application on it.
+    // We select the first element on the page matching the CSS selector `main` and start the
+    // application on it.
     draco::start(HelloWorld, draco::select("main").expect("<main>").into());
 }
