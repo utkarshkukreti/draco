ea86a422369b3b580689de7df9bd1f0f103574d9
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-10-21 09:20:27 +0530

    feat(examples/router_basic)

---
+++ examples/router_basic/src/lib.rs
@@ -0,0 +1,76 @@
+use draco::url::Url;
+use wasm_bindgen::prelude::*;
+
+struct Router {
+    url: Url,
+}
+
+impl Router {
+    fn new() -> Self {
+        Router {
+            url: draco::router::current_url(draco::router::Mode::Hash),
+        }
+    }
+}
+
+enum Message {
+    Navigate(Url),
+    NoOp,
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        Message::NoOp
+    }
+}
+
+impl draco::Application for Router {
+    type Message = Message;
+
+    fn update(&mut self, message: Self::Message, _mailbox: &draco::Mailbox<Self::Message>) {
+        match message {
+            Message::Navigate(url) => {
+                self.url = url;
+            }
+            Message::NoOp => {}
+        }
+    }
+
+    fn view(&self) -> draco::Node<Self::Message> {
+        use draco::html as h;
+        use draco::router::Mode::Hash;
+        let urls = ["/foo", "/foo/bar", "/foo/bar?baz=quux", "/foo/bar?baz#quux"];
+
+        h::div()
+            .push(h::h3().push(format!("Current Url: {:?}", &self.url)))
+            .append(urls.iter().map(|url| {
+                h::div()
+                    .attribute(
+                        "style",
+                        if url == &self.url.to_string() {
+                            "padding: .25rem .75rem; background: #fefcbf; border: 1px solid #ecc94b; border-radius: 4px;"
+                        } else {
+                            "padding: .25rem .75rem;"
+                        },
+                    )
+                    .push(
+                        h::span().push(
+                            draco::router::link(Hash, Url::from(*url))
+                                .push(url.to_string())
+                                .attribute("style", "margin-right: .5rem;"),
+                        ),
+                    )
+            }))
+            .into()
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    let mailbox = draco::start(Router::new(), draco::select("main").expect("<main>").into());
+
+    mailbox.subscribe_forever(
+        draco::router::Router::new(draco::router::Mode::Hash),
+        Message::Navigate,
+    );
+}
