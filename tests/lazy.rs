41fc729dfd4fc030927556521ea4fce614902f8f
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-10-26 10:16:42 +0530

    tests(lazy): add failing test for when arg is same but function is diff

---
+++ tests/lazy.rs
@@ -0,0 +1,31 @@
+use draco::{Lazy, Mailbox, Node};
+use wasm_bindgen::{JsCast, UnwrapThrowExt};
+use wasm_bindgen_test::*;
+use web_sys as web;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn t_same_arg_but_different_function() {
+    let mailbox = Mailbox::new(|_| ());
+    let mut node_1: Node<()> = Lazy::new(1, |&x| x.to_string().into()).into();
+    let web_node = node_1.create(&mailbox);
+    assert_eq!(
+        web_node
+            .dyn_into::<web::Text>()
+            .unwrap_throw()
+            .text_content()
+            .unwrap_throw(),
+        "1"
+    );
+    let mut node_2: Node<()> = Lazy::new(1, |&x| (x + 1).to_string().into()).into();
+    let web_node = node_2.patch(&mut node_1, &mailbox);
+    assert_eq!(
+        web_node
+            .dyn_into::<web::Text>()
+            .unwrap_throw()
+            .text_content()
+            .unwrap_throw(),
+        "2"
+    );
+}
